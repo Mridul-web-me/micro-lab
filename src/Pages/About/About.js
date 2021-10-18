39a13f3b178956715daf67d4459f5df1fd8edc26
Mridul-web-me <md.mridu.23@gmail.com>
2021-10-19 01:55:25 +0600

    about added

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,9 +1,68 @@
 import React from 'react'
-
+import './About.css'
 const About = () => {
     return (
-        <div>
-            This Is About
+        <div className="about">
+            <div className="container">
+                <h1 className="aboutHeading text-center">About Us</h1>
+
+                <div class="row row-cols-1 row-cols-md-3 g-4">
+                    <div class="col">
+                        <div class="card">
+                            <i class="fas fa-map"></i>
+                            <div class="card-body">
+                                <h5 class="card-title">Advanced medical technologies</h5>
+                                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.This content is a little bit longer.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card">
+                            <i class="fas fa-rocket"></i>
+                            <div class="card-body">
+                                <h5 class="card-title">High professionalism of specialists</h5>
+                                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.This content is a little bit longer.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card">
+                            <i class="fas fa-gift"></i>
+                            <div class="card-body">
+                                <h5 class="card-title">High professionalism of specialists</h5>
+                                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card">
+                            <i class="fas fa-paper-plane"></i>
+                            <div class="card-body">
+                                <h5 class="card-title">High quality of medical services</h5>
+                                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.This content is a little bit longer.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card">
+                            <i class="fas fa-glasses"></i>
+                            <div class="card-body">
+                                <h5 class="card-title">Optimal prices and quality</h5>
+                                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.This content is a little bit longer.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card">
+                            <i class="fas fa-ribbon"></i>
+                            <div class="card-body">
+                                <h5 class="card-title">Optimal prices and quality</h5>
+                                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.This content is a little bit longer.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -8,10 +8,70 @@ const Home = () => {
     return (
         <div>
             <Banner>
-
             </Banner>
-
             <GroupCard></GroupCard>
+            <div className="about">
+                <div className="container">
+                    <h1 className=" aboutHeading text-center">About Us</h1>
+
+                    <div class="row row-cols-1 row-cols-md-3 g-4">
+                        <div class="col">
+                            <div class="card">
+                                <i class="fas fa-map"></i>
+                                <div class="card-body">
+                                    <h5 class="card-title">Advanced medical technologies</h5>
+                                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.This content is a little bit longer.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="card">
+                                <i class="fas fa-rocket"></i>
+                                <div class="card-body">
+                                    <h5 class="card-title">High professionalism of specialists</h5>
+                                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.This content is a little bit longer.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="card">
+                                <i class="fas fa-gift"></i>
+                                <div class="card-body">
+                                    <h5 class="card-title">High professionalism of specialists</h5>
+                                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="card">
+                                <i class="fas fa-paper-plane"></i>
+                                <div class="card-body">
+                                    <h5 class="card-title">High quality of medical services</h5>
+                                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.This content is a little bit longer.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="card">
+                                <i class="fas fa-glasses"></i>
+                                <div class="card-body">
+                                    <h5 class="card-title">Optimal prices and quality</h5>
+                                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.This content is a little bit longer.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="card">
+                                <i class="fas fa-ribbon"></i>
+                                <div class="card-body">
+                                    <h5 class="card-title">Optimal prices and quality</h5>
+                                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.This content is a little bit longer.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
 
 
